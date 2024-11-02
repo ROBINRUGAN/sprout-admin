525ad216a461b1f4f2df003a3824fceef2a323d4
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-02 22:38:00 +0800

    add: 构建登录页左侧内容

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,7 +13,12 @@ const loginData = ref({
   <div class="background">
     <el-row class="wrapper">
       <el-col :span="12" class="left">
-        <!-- 左侧内容待添加 -->
+        <div class="info">
+          <p style="font-size: 32px">新苗同学</p>
+          <p>你的大学生活助手</p>
+          <p>大学原来如此有趣</p>
+        </div>
+        <img class="pic" src="../assets/login/pic.png" alt="mewch" />
       </el-col>
       <el-col :span="12" class="right">
         <!-- 右侧内容待添加 -->
