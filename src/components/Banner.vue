80b377e146584256e1535e855453b95dbfd03f75
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-02 23:02:31 +0800

    add: 添加导航栏和侧边栏

---
+++ src/components/Banner.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container class="app-header">
+    <el-header>
+      <div class="header-logo">
+        <img src="../assets/login/logo.png" alt="Logo" />
+        <span style="font-size: 20px; font-family: AlibabaPuHui-medium">新苗同学</span>
+      </div>
+    </el-header>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+import { ElContainer, ElHeader } from 'element-plus'
+</script>
+
+<style scoped>
+.app-header .el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  box-shadow: 0px 6px 10px 0px rgba(78, 89, 105, 0.06);
+  width: 100%;
+}
+
+.header-logo {
+  display: flex;
+  align-items: center;
+}
+
+.header-logo img {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-menu
+    router
+    :default-active="activeIndex"
+    :collapse="isCollapsed"
+    collapse-transition
+    class="el-menu-vertical-demo"
+  >
+    <el-menu-item index="/home/dashboard">
+      <el-icon><Odometer /></el-icon>
+      <template #title>工作台</template>
+    </el-menu-item>
+
+    <el-menu-item index="/home/addStuds">
+      <el-icon><BottomRight /></el-icon>
+      <template #title>学生导入</template>
+    </el-menu-item>
+
+    <el-menu-item index="/home/add">
+      <el-icon><DocumentAdd /></el-icon>
+      <template #title>发布任务</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const isCollapsed = ref(true)
+
+const activeIndex = computed(() => {
+  return route.path.startsWith('/home/dashboard') ? '/home/dashboard' : route.path
+})
+</script>
+
+<style lang="scss" scoped>
+.el-menu-vertical-demo {
+  transition: all ease;
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
