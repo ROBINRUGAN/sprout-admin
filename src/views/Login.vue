e10cadff3fa929889a8e5668a7adbf42cff207e6
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-02 22:43:55 +0800

    feat: 增加登陆账号密码输入

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,7 +21,27 @@ const loginData = ref({
         <img class="pic" src="../assets/login/pic.png" alt="mewch" />
       </el-col>
       <el-col :span="12" class="right">
-        <!-- 右侧内容待添加 -->
+        <img class="logo" src="../assets/login/logo.png" alt="" />
+        <div class="wrapper2">
+          <p style="font-size: 28px; margin: 20px 0 10px 20px">新苗同学</p>
+          <div class="form">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              v-model="loginData.username"
+              placeholder="账号"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              v-model="loginData.password"
+              placeholder="密码"
+            />
+            <button @click="authStore.Login(loginData)">登录</button>
+          </div>
+        </div>
       </el-col>
     </el-row>
   </div>
