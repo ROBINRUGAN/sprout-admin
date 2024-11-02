8a8720b3b6c5df058ef9f071b3aedcfe085a5e9a
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-02 22:28:26 +0800

    init: 初始化登陆页面结构

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
+
+const authStore = useAuthStore()
+</script>
+
+<template>
+  <div class="background">
+    <el-row class="wrapper">
+      <el-col :span="12" class="left">
+        <!-- 左侧内容待添加 -->
+      </el-col>
+      <el-col :span="12" class="right">
+        <!-- 右侧内容待添加 -->
+      </el-col>
+    </el-row>
+  </div>
+</template>
