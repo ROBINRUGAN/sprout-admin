300b8fd5392bbdda9ec37af971fbca11a3b2e9cf
吴荣榜 <1220045015@qq.com>
2024-11-03 17:33:53 +0800

    add: 实现活动审核搜索表单部分

---
+++ src/views/CheckActivity.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="wrapper">
+    <h1 style="font-size: 24px; margin-top: 15px; margin-left: 15px; margin-bottom: 15px">
+      学生提交审核
+    </h1>
+    <el-form :model="form" label-width="auto" style="margin-top: 20px" @submit.prevent="search">
+      <el-row :gutter="20">
+        <!-- 活动名称 -->
+        <el-col :span="8">
+          <el-form-item label="活动名称">
+            <el-input v-model="searchForm.keyword" placeholder="活动名称"></el-input>
+          </el-form-item>
+        </el-col>
+        <!-- 活动类型 -->
+        <el-col :span="12">
+          <el-form-item label="活动类型">
+            <el-radio-group v-model="searchForm.queryParentTask">
+              <el-radio :value="1" label="父任务"></el-radio>
+              <el-radio :value="0" label="单项任务"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item>
+            <el-button type="primary" @click="search">查询</el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+
+    <el-divider />
+
+</template>
+
+<style scoped>
+.wrapper {
+  border-radius: 4px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  padding: 20px;
+}
+.el-button--primary {
+  width: 100px;
+}
+.el-divider--horizontal {
+  margin: 0;
+}
+
+.scroll-container {
+  overflow-x: auto;
+  white-space: nowrap;
+  padding: 16px 0;
+}
+
+.card {
+  display: inline-block;
+  width: 450px; /* 这里的宽度可以根据您的设计来调整 */
+  margin-right: 16px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  vertical-align: top;
+}
+.card-image {
+  width: 450px;
+  height: 250px;
+  object-fit: cover;
+  display: block;
+}
+
+.card-content {
+  padding: 16px;
+}
+
+.card-title {
+  font-size: 1.2em;
+  margin-bottom: 8px;
+}
+
+.card-description {
+  font-size: 0.9em;
+  color: #666;
+}
+.card-id {
+  justify-content: right;
+  font-size: 0.5em;
+  margin-top: -5px;
+  margin-bottom: 10px;
+  padding: 0;
+  color: #666;
+  display: flex;
+}
+</style>
