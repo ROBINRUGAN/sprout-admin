1f99e04ee849fa5d18eefb50e6f95c94a85ebf7b
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-03 16:12:17 +0800

    add: 增加学生导入页面

---
+++ src/views/AddStudent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { ElMessage, ElUpload } from 'element-plus'
+import { UploadFilled } from '@element-plus/icons-vue'
+
+const fileList = ref([]) // 存储用户选择的文件
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1 style="font-size: 24px; margin-top: 15px; margin-left: 15px; margin-bottom: 15px">
+      学生批量录入
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  border-radius: 4px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  padding: 20px;
+}
+</style>
