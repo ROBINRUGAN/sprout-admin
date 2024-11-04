98bb06fd2fc6e5c4f964a232957a2e63f327fca8
吴荣榜 <1220045015@qq.com>
2024-11-04 17:32:58 +0800

    feat: 添加往期活动的详细信息查询功能

--- src/views/Past.vue
+++ src/views/Past.vue
@@ -94,6 +94,46 @@ const search = () => {
   })
 }
 
+const getDetail = (data: any, index: number) => {
+  getPastChildApi({
+    id: data.id,
+    querySubTask: 1
+  }).then((res) => {
+    if (res.data.code == '0') {
+      ElMessage.success('查询成功')
+      detailItems.value = res.data.data.subTaskList
+      showSon.value = true
+      if (data.parentId == '0') {
+        showDetail.value = true
+        form.value = data
+        if (form.value.requiresAudit === 1) {
+          form.value.Audit = true
+        } else {
+          form.value.Audit = false
+        }
+        form.value.water = parseInt(form.value.taskRewards.split(',')[0])
+        form.value.chan = parseInt(form.value.taskRewards.split(',')[1])
+        form.value.tree = parseInt(form.value.taskRewards.split(',')[2])
+      }
+    } else {
+      ElMessage.error(res.data.message)
+    }
+  })
+}
+
+const getDetailInfo = (id: number) => {
+  showDetail.value = true
+  form.value = detailItems.value[id]
+  if (form.value.requiresAudit === 1) {
+    form.value.Audit = true
+  } else {
+    form.value.Audit = false
+  }
+  form.value.water = parseInt(items.value[id].taskRewards.split(',')[0])
+  form.value.chan = parseInt(items.value[id].taskRewards.split(',')[1])
+  form.value.tree = parseInt(items.value[id].taskRewards.split(',')[2])
+}
+
 onMounted(() => {
   search()
 })
