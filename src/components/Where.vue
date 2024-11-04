10b8f9205400c2d497709654d9bee9fb61116a12
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-04 12:59:21 +0800

    feat: 地图注册和配置选项

--- src/components/Where.vue
+++ src/components/Where.vue
@@ -9,6 +9,65 @@ import chinaMapData from '@/assets/china.json' // 请确保正确引入中国地
 
 const whereContainer = ref(null)
 let myChart = null
+
+echarts.registerMap('china', chinaMapData)
+
+const option = {
+  title: {
+    text: '新生注册位置统计',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item',
+    backgroundColor: 'rgba(236,245,253,0.7)',
+    formatter: function (params) {
+      let result = '<h1>' + params.name + '</h1>'
+      result +=
+        '<div style="background: #fff; padding: 10px; border-radius: 5px; margin-top: 5px; width:200px; display: flex; justify-content: space-between">'
+      result += '<p>'
+      result += params.seriesName
+      result += '</p>'
+      result += '<p>'
+      result += params.value + '<br/>'
+      result += '</p>'
+      result += '</div>'
+      return result
+    }
+  },
+  visualMap: {
+    min: 50,
+    max: 270,
+    left: 'left',
+    top: 'top',
+    text: ['高', '低'],
+    realtime: true,
+    calculable: true,
+    inRange: {
+      color: ['rgba(222,244,241,1)', 'rgba(0, 128, 102, 1)'] // 色阶从浅绿到深绿
+    }
+  },
+  series: [
+    {
+      name: '人数',
+      type: 'map',
+      mapType: 'china',
+      roam: false, // 允许缩放和平移
+      layoutCenter: ['50%', '70%'], // 地图位置
+      layoutSize: '120%', // 地图大小
+      label: {
+        show: false,
+        color: 'rgba(0,0,0,0.6)'
+      },
+      data: [
+        { name: '北京市', value: 217 },
+        { name: '天津市', value: 195 },
+        { name: '上海市', value: 240 },
+        { name: '重庆市', value: 182 },
+        // ...其他省份数据
+      ]
+    }
+  ]
+}
 </script>
 
 <style scoped>
