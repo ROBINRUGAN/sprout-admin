2a2f653039e60d9123c0eb154503c168708ef4bc
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-04 14:22:02 +0800

    style: 调整新生注册统计地区图标组件样式并添加参数

--- src/components/Where.vue
+++ src/components/Where.vue
@@ -63,13 +63,48 @@ const option = {
         { name: '天津市', value: 195 },
         { name: '上海市', value: 240 },
         { name: '重庆市', value: 182 },
-        // ...其他省份数据
+        { name: '河北省', value: 160 },
+        { name: '河南省', value: 180 },
+        { name: '云南省', value: 150 },
+        { name: '辽宁省', value: 210 },
+        { name: '黑龙江省', value: 130 },
+        { name: '湖南省', value: 195 },
+        { name: '安徽省', value: 175 },
+        { name: '山东省', value: 220 },
+        { name: '新疆维吾尔自治区', value: 140 },
+        { name: '江苏省', value: 230 },
+        { name: '浙江省', value: 210 },
+        { name: '江西省', value: 160 },
+        { name: '湖北省', value: 205 },
+        { name: '广西壮族自治区', value: 180 },
+        { name: '甘肃省', value: 120 },
+        { name: '山西省', value: 170 },
+        { name: '内蒙古自治区', value: 110 },
+        { name: '陕西省', value: 190 },
+        { name: '吉林省', value: 140 },
+        { name: '福建省', value: 195 },
+        { name: '贵州省', value: 165 },
+        { name: '广东省', value: 250 },
+        { name: '青海省', value: 95 },
+        { name: '西藏自治区', value: 85 },
+        { name: '四川省', value: 220 },
+        { name: '宁夏回族自治区', value: 105 },
+        { name: '海南省', value: 135 },
+        { name: '台湾省', value: 230 },
+        { name: '香港特别行政区', value: 215 },
+        { name: '澳门特别行政区', value: 80 }
       ]
     }
   ]
 }
+window.addEventListener('resize', function () {
+  myChart.resize()
+})
+onMounted(() => {
+  myChart = echarts.init(whereContainer.value)
+  myChart.setOption(option)
+})
 </script>
-
 <style scoped>
 .whereWrapper {
   background-color: rgba(255, 255, 255, 1);
