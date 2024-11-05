4e9c5d47eb5c428b55f01f809e814199124faf8b
吴荣榜 <1220045015@qq.com>
2024-11-05 14:13:40 +0800

    add: 添加广告模块选择框

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -12,6 +12,15 @@
         <h1>广告统计</h1>
         <p style="font-size: 12px">(近七日)</p>
       </div>
+
+      <el-select v-model="state.selectedAdType" placeholder="选择广告类型" style="width: 200px">
+        <el-option
+          v-for="item in state.adTypes"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
     </div>
 
     <div ref="adContainer" style="width: 100%; height: 300px"></div>
