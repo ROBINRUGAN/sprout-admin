2ef48dacea7904a72ecb0a828380f4f1a8170e23
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-05 17:26:41 +0800

    style: 创建广告页面样式调整

--- src/views/CreateAd.vue
+++ src/views/CreateAd.vue
@@ -184,3 +184,11 @@ onBeforeMount(() => {
   fetchColleges()
 })
 </script>
+<style scoped>
+.wrapper {
+  border-radius: 4px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  height: 85vh;
+}
+</style>
