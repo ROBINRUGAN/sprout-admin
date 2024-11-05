68a120ae0bb368a5cb9a0235948a7d4ad310a0ef
吴荣榜 <1220045015@qq.com>
2024-11-05 13:12:40 +0800

    feat: 编写广告组件顶部元素

---
+++ src/components/Ad.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="adData">
+    <div
+      style="
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 30px;
+      "
+    >
+      <div style="display: flex; justify-content: space-between; width: 125px; align-items: center">
+        <h1>广告统计</h1>
+        <p style="font-size: 12px">(近七日)</p>
+      </div>
+    </div>
+
+    <div ref="adContainer" style="width: 100%; height: 300px"></div>
+  </div>
+</template>
