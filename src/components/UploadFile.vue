2d73804e121e2be4ae77ec65a3fc5dff9f72c449
吴荣榜 <1220045015@qq.com>
2024-11-06 12:38:58 +0800

    add: 新增上传文件组件

---
+++ src/components/UploadFile.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-upload
+    v-model:file-list="fileList"
+    :http-request="uploadImage"
+    list-type="picture"
+    :before-upload="beforeUpload"
+    :on-preview="handlePictureCardPreview"
+    :on-remove="handleRemove"
+    :on-success="handleSuccess"
+    :on-change="handleChange"
+    :limit="2"
+    :auto-upload="true"
+  >
+    <el-button type="primary">点击上传</el-button>
+    <template #tip>
+      <div class="el-upload__tip">支持上传jpg/png/avi/mov/mp4文件，不超过50M</div>
+    </template>
+  </el-upload>
+</template>
