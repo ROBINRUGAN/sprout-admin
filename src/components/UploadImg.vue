ea886c87c309be46825dbbfa80a3194fbec54567
“CZX” <fidyreowtifseowgdarew@site2022.ddnsfree.com>
2024-11-07 11:58:46 +0800

    add: 增加文件上传组件

---
+++ src/components/UploadImg.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-upload
+    v-model:file-list="fileList"
+    list-type="picture-card"
+    :on-preview="handlePictureCardPreview"
+    :on-remove="handleRemove"
+  >
+    <el-icon><Plus /></el-icon>
+  </el-upload>
+
+  <el-dialog v-model="dialogVisible">
+    <img w-full :src="dialogImageUrl" alt="Preview Image" style="width: 100%" />
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Plus } from '@element-plus/icons-vue'
+import type { UploadUserFile } from 'element-plus'
+
+const fileList = ref<UploadUserFile[]>([])
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+
+const handlePictureCardPreview = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url
+  dialogVisible.value = true
+}
+
+const handleRemove = (uploadFile, uploadFiles) => {
+  // handle file removal if needed
+}
+</script>
