0c756bad8181285d86b09d7efc585a954b4178e2
吴荣榜 <1220045015@qq.com>
2024-11-15 00:09:07 +0800

    feat: 实现官网页面

--- src/views/Official.vue
+++ src/views/Official.vue
@@ -2,22 +2,41 @@
   <!-- Part 1 -->
   <div class="part1">
     <div class="container">
-      <img src="@/assets/login/logo.png" class="logo" />
-      <h1>新苗同学</h1>
-      <h2>你的大学生活助手</h2>
+      <img src="../assets/login/logo.png" class="logo" />
+      <img src="../assets/official/title.png" style="width: 660px" />
+      <h2
+        style="
+          background: linear-gradient(to right, #0090f7, #ba62fc, #f2416b);
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          font-size: 28px;
+          margin: 20px 0;
+        "
+      >
+        你的智能大学生活助手
+      </h2>
       <el-carousel :height="carouselHeight" class="carousel" indicator-position="none">
-        <el-carousel-item v-for="item in 4" :key="item">
-          <img :src="`./src/assets/official/carousel${item}.png`" class="carousel-img" />
+        <el-carousel-item>
+          <img src="../assets/official/carousel1.png" class="carousel-img" />
+        </el-carousel-item>
+        <el-carousel-item>
+          <img src="../assets/official/carousel2.png" class="carousel-img" />
+        </el-carousel-item>
+        <el-carousel-item>
+          <img src="../assets/official/carousel3.png" class="carousel-img" />
+        </el-carousel-item>
+        <el-carousel-item>
+          <img src="../assets/official/carousel4.png" class="carousel-img" />
         </el-carousel-item>
       </el-carousel>
       <div class="entrance">
         <div class="android" @click="downloadAndroid">
-          <img src="@/assets/official/android.svg" />
+          <img src="../assets/official/android.svg" />
           <span>Download for Android</span>
         </div>
         <div class="manage" @click="$router.push('/login')">
           <span>Enter Manage Center</span>
-          <img src="@/assets/official/manage.png" />
+          <img src="../assets/official/manage.png" />
         </div>
       </div>
     </div>
@@ -25,7 +44,16 @@
 
   <!-- Part 2 -->
   <div class="part2">
-    <h1>强大、有趣、稳定、便捷。</h1>
+    <h1
+      style="
+        background: linear-gradient(to right, #0090f7, #ba62fc, #f2416b);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-size: 28px;
+      "
+    >
+      强大、有趣、稳定、便捷。
+    </h1>
     <div class="grid">
       <div v-for="(intro, index) in intros" :key="index" class="intro-item">
         <img :src="intro.img" alt="Intro Image" class="intro-img" />
@@ -37,24 +65,24 @@
 
   <!-- Part 3 -->
   <div class="part3">
-    <img src="@/assets/official/sprout.png" style="width: 30%; min-width: 250px" />
-    <img class="group" src="@/assets/official/group_phone.png" />
+    <!-- <img src="../assets/official/sprout.png" style="width: 30%; min-width: 250px" /> -->
+    <img class="group" src="../assets/official/group_phone.png" />
     <div>
       <div class="devices">
         <div>
-          <img src="@/assets/official/client.png" />
+          <img src="../assets/official/client.png" />
           <p>客户端</p>
         </div>
         <div>
-          <img src="@/assets/official/admin.png" />
+          <img src="../assets/official/admin.png" />
           <p>管理端</p>
         </div>
         <div>
-          <img src="@/assets/official/more.png" />
+          <img src="../assets/official/more.png" />
           <p>敬请期待</p>
         </div>
       </div>
-      <img src="@/assets/official/group_web.png" style="width: 60%" />
+      <img src="../assets/official/group_web.png" style="width: 60%" />
     </div>
   </div>
 
@@ -66,7 +94,7 @@
       <p><span class="number">88+</span><span class="desc">五星好评</span></p>
     </div>
     <p class="contact">联系我们</p>
-    <p>Sprout新苗团队</p>
+    <p style="color: gray; font-weight: bold">Sprout新苗团队</p>
     <p class="footer">地址：福建省福州市福州大学旗山校区</p>
     <p class="footer">©Sprout新苗 版权所有</p>
   </div>
@@ -165,7 +193,7 @@
   white-space: pre-line;
 }
 .grid {
-  margin-top: 80px;
+  margin-top: 10px;
   display: grid;
   grid-template-columns: repeat(3, 1fr); /* 每行 3 列 */
   gap: 20px;
@@ -191,7 +219,10 @@
 .grid h3 {
   margin: 15px;
 }
-
+.part2,
+.part3 {
+  margin-bottom: 0;
+}
 .part3 .group {
   width: 80%;
 }
@@ -257,7 +288,7 @@
 .footer {
   color: gray;
   font-size: small;
-  margin-top: 25px;
+  margin-top: 10px;
 }
 </style>
 <script lang="ts" setup>
@@ -286,35 +317,36 @@ interface Intro {
   title: string
   content: string
 }
+
 const intros = ref<Intro[]>([
   {
-    img: '../src/assets/official/game.png',
+    img: new URL('../assets/official/game.png', import.meta.url).href,
     title: '游戏化任务',
     content: '创新的游戏化设计\n提升用户体验'
   },
   {
-    img: '../src/assets/official/AI.png',
+    img: new URL('../assets/official/AI.png', import.meta.url).href,
     title: 'AI识别',
     content: '支持人脸识别、姿态识别、物品识别等AI识别功能'
   },
   {
-    img: '../src/assets/official/earn.png',
+    img: new URL('../assets/official/earn.png', import.meta.url).href,
     title: '多元盈利',
     content: '多元化盈利模式\n具有商业可持续性'
   },
   {
-    img: '../src/assets/official/fullsvc.png',
+    img: new URL('../assets/official/fullsvc.png', import.meta.url).href,
     title: '全面服务',
     content:
       '支持查询学校信息、报修设备、在活动评论区交流、查询个人成绩排名、参考选课意见等全面校园服务'
   },
   {
-    img: '../src/assets/official/vsbdata.png',
+    img: new URL('../assets/official/vsbdata.png', import.meta.url).href,
     title: '可视化数据',
     content: '管理端提供日活、广告、学生注册地区、学生学院等信息的可视化查询功能'
   },
   {
-    img: '../src/assets/official/managable.png',
+    img: new URL('../assets/official/managable.png', import.meta.url).href,
     title: '支持管理',
     content: '管理员可以通过管理员账号在PC端、手机端管理平台信息'
   }
